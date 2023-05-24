2936313f4be387142f5d9a9717262f0009c1bebf
김동인_Aaron <dongin.kim@lemonhc.com>
2023-05-24 10:52:11 +0900

    second commit

---
+++ src/methods/filter.js
@@ -0,0 +1,22 @@
+    module.exports.authenticate = function(req, res, next) {
+        console.log('authenticating')
+        console.log(req.headers.authorization)
+        
+          var headerExists = req.headers.authorization;
+        
+          if (headerExists) {
+            var token = req.headers.authorization.split(' ')[1]; //--> Authorization Bearer xxx
+            jwt.verify(token, 'passwordgoeshere', function(error, decoded) {
+              if (error) {
+                console.log(error);
+                res.status(401).json('Unauthorized');
+              } else {
+                req.user = decoded.username;
+                var authenticated = true;
+                next();
+              }
+            });
+          } else {
+            res.status(403).json('No token provided');
+          }
+        };
